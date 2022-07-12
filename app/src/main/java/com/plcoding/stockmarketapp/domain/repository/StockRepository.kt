ebfd83db85381f641a1444a17e968ce26072283b
robbmoskv <fabrizio_39@hotmail.com>
2022-07-12 12:07:28 +0200

    Define Repository Interface to load company listings

---
+++ app/src/main/java/com/plcoding/stockmarketapp/domain/repository/StockRepository.kt
@@ -0,0 +1,15 @@
+package com.plcoding.stockmarketapp.domain.repository
+
+import androidx.room.Query
+import com.plcoding.stockmarketapp.domain.model.CompanyListing
+import com.plcoding.stockmarketapp.util.Resource
+import kotlinx.coroutines.flow.Flow
+
+interface StockRepository {
+
+    // Load data from source. Either remote or local
+    suspend fun getCompanyListings(
+        fetchFromRemote: Boolean,
+        query: String
+    ): Flow<Resource<List<CompanyListing>>>
+}
