c73d0b9ef451c961a7374b78b50249ee485bbce3
robbmoskv <fabrizio_39@hotmail.com>
2022-08-01 13:29:43 +0200

    Extend Stock API Repository Interface with new company info requests

--- app/src/main/java/com/plcoding/stockmarketapp/domain/repository/StockRepository.kt
+++ app/src/main/java/com/plcoding/stockmarketapp/domain/repository/StockRepository.kt
@@ -1,14 +1,23 @@
 package com.plcoding.stockmarketapp.domain.repository
 
+import com.plcoding.stockmarketapp.domain.model.CompanyInfo
 import com.plcoding.stockmarketapp.domain.model.CompanyListing
+import com.plcoding.stockmarketapp.domain.model.IntradayInfo
 import com.plcoding.stockmarketapp.util.Resource
 import kotlinx.coroutines.flow.Flow
 
 interface StockRepository {
 
-    // Load data from source. Either remote or local
     suspend fun getCompanyListings(
         fetchFromRemote: Boolean,
         query: String
     ): Flow<Resource<List<CompanyListing>>>
+
+    suspend fun getIntradayInfoBySymbol(
+        symbol: String
+    ): Resource<List<IntradayInfo>>
+
+    suspend fun getCompanyInfoBySymbol(
+        symbol: String
+    ): Resource<CompanyInfo>
 }
