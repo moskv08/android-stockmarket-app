beb53d761e7e0a909519d1e03ea24f6cdc67143f
robbmoskv <fabrizio_39@hotmail.com>
2022-08-01 10:29:02 +0200

    Define mapper from DTO to domain model object

--- app/src/main/java/com/plcoding/stockmarketapp/data/mapper/IntradayInfoMapper.kt
+++ app/src/main/java/com/plcoding/stockmarketapp/data/mapper/IntradayInfoMapper.kt
@@ -1,4 +1,18 @@
 package com.plcoding.stockmarketapp.data.mapper
 
-class IntradayInfoMapper {
+import com.plcoding.stockmarketapp.data.remote.dto.IntradayInfoDto
+import com.plcoding.stockmarketapp.domain.model.IntradayInfo
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
+
+fun IntradayInfoDto.toIntradayInfo(): IntradayInfo {
+    val dateTimePattern = "yyyy-MM-dd HH:mm:ss"
+    val formatter = DateTimeFormatter.ofPattern(dateTimePattern, Locale.getDefault())
+    val localDateTime = LocalDateTime.parse(timestamp, formatter)
+
+    return IntradayInfo(
+        date = localDateTime,
+        close = close
+    )
 }
